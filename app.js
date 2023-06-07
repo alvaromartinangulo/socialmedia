c2e719fd096ec1d392b0dd3929a2464332e35ce4
alvaromartinangulo <martinangulo.alvaro@gmail.com>
2023-06-07 18:16:46 +0200

    Connection to database

---
+++ app.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+const uri = require("./atlas_uri");
+
+console.log(uri);
+
+const client = new MongoClient(uri);
+const dbname = "socialmedia";
+
+const connectToDataBase = async () =>{
+    try {
+        await client.connect();
+        console.log(`Connected to the ${dbname} database`);
+    } catch (err) {
+        console.error(`Error connecting to the database: ${err}`);
+    }
+}
+
+const main = async () =>{
+    try{
+        await connectToDataBase();
+    } catch(err){
+        console.error(`Error connecting to the database: ${err}`)
+    } finally{
+        await client.close();
+    }
+}
+
+main();
---
+++ atlas_uri.js
@@ -0,0 +1,2 @@
+module.exports = uri = 
+"mongodb+srv://admin:dCcxtBIuQS2o9rga@cluster0.vwuh4yo.mongodb.net/?retryWrites=true&w=majority"
