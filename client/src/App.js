7cc7bd4cd3604e6e6d83c5c7bedeb3cf4e339367
alvaromartinangulo <martinangulo.alvaro@gmail.com>
2023-06-12 21:00:22 +0200

    Front end from scratch

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Landing from "./components/layout/Landing.js";
-import Register from "./components/Auth/Register.js";
-import Login from "./components/Auth/Login.js";
+import "./App.css";
+import Navbar from "./components/Layout/Navbar";
+import Landing from "./components/Layout/Landing";
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </div>
-      </Router>
+      <div className="App">
+        <Navbar />
+        <Landing />
+      </div>
     );
   }
 }
