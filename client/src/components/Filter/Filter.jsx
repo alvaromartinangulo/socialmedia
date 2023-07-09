d2b6cbcf04a3ff47922fb4e8136653eedafbf49d
alvaromartinangulo <martinangulo.alvaro@gmail.com>
2023-07-09 20:43:08 +0200

    Filter working, need to apply filter to posts and styling

---
+++ client/src/components/Filter/Filter.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import "./Filter.css";
+
+const filterParams = [
+  "All Categories",
+  "Outerwear",
+  "Sweatshirts",
+  "T-Shirts & Polos",
+  "Bottoms",
+  "Sunglasses",
+  "Footwear",
+  "Headwear",
+  "Bags",
+  "Accessories"
+];
+
+const Filter = () => {
+  const [selectedFilters, setSelectedFilters] = useState(new Set(["All Categories"]));
+  const [isAllSelected, setIsAllSelected] = useState(true);
+
+  const handleSelect = (filterParam) => {
+    switch (filterParam) {
+      case "All Categories":
+        if (!isAllSelected) {
+          setIsAllSelected(true);
+          setSelectedFilters(new Set(["All Categories"]));
+        }
+        break; // Add a break statement here
+
+      default:
+        if (selectedFilters.has(filterParam)) {
+          const updatedFilters = new Set(selectedFilters);
+          updatedFilters.delete(filterParam);
+          setSelectedFilters(updatedFilters);
+
+          if (updatedFilters.size === 0) {
+            setIsAllSelected(true);
+            setSelectedFilters(new Set(["All Categories"]));
+          }
+        } else {
+          const updatedFilters = new Set(selectedFilters);
+          updatedFilters.add(filterParam);
+          setSelectedFilters(updatedFilters);
+
+          if (isAllSelected) {
+            setIsAllSelected(false);
+            updatedFilters.delete("All Categories");
+            setSelectedFilters(updatedFilters);
+          }
+        }
+    }
+  };
+
+  return (
+    <div className="Filter">
+        <ul> {/* Wrap the list items in a ul element */}
+          {filterParams.map((filterParam, id) => {
+            const isSelected = selectedFilters.has(filterParam);
+            return (
+              <li key={id}>
+                <button
+                  className={isSelected ? "button selected" : "button not-selected"}
+                  onClick={() => handleSelect(filterParam)}
+                >
+                  {filterParam}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+    </div>
+  );
+};
+
+export default Filter;
--- client/src/components/Following/Following.jsx
+++ client/src/components/Following/Following.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Posts from "../Posts/Posts"
 import "./Following.css"
+import Filter from "../Filter/Filter";
 const Following = () =>{
     return (
         <div className="Following">
+            <Filter/>
             <Posts/>
         </div>
     )
